26ffeb7c912d7286a8ba8111cce5819884ef7e2b
Julia Poluyan <90253693+Vamprsii@users.noreply.github.com>
2022-10-29 10:37:55 +0300

    Вычисление площади треугольника

---
+++ laba2/Lab1.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class Lab1 {
+    public static void main(String[] args) throws IOException {
+        Point3d myPoint1 = new Point3d(Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
+        Point3d myPoint2 = new Point3d(Double.parseDouble(args[3]), Double.parseDouble(args[4]), Double.parseDouble(args[5]));
+        Point3d myPoint3 = new Point3d(Double.parseDouble(args[6]), Double.parseDouble(args[7]), Double.parseDouble(args[8]));
+        if (Point3d.comparePoint3d(myPoint1, myPoint2) || Point3d.comparePoint3d(myPoint1, myPoint3) || Point3d.comparePoint3d(myPoint2, myPoint3)) {
+            System.out.println("Невозможно найти площадь");
+        } else {
+            System.out.println(computerArea(myPoint1, myPoint2, myPoint3));
+        }
+        public static double computerArea (Point3d myPint1, Point3d myPint2, Point3d myPint3){
+            double AB = Math.sqrt(Math.pow(myPint1.getX() - myPint2.getX(), 2) + Math.pow(myPint1.getY() - myPint2.getY(), 2) + Math.pow(myPint1.getZ() - myPint1.getZ(), 2));
+            double BC = Math.sqrt(Math.pow(myPint2.getX() - myPint3.getX(), 2) + Math.pow(myPint2.getY() - myPint3.getY(), 2) + Math.pow(myPint2.getZ() - myPint3.getZ(), 2));
+            double CA = Math.sqrt(Math.pow(myPint3.getX() - myPint1.getX(), 2) + Math.pow(myPint3.getY() - myPint1.getY(), 2) + Math.pow(myPint3.getZ() - myPint1.getZ(), 2));
+            double p = (AB + BC + CA) / 2;
+            double s = Math.sqrt(p * (p - AB) * (p - BC) * (p - CA));
+            return s;
+        }
+    }
+}
---
+++ laba2/Point3d.java
@@ -0,0 +1,36 @@
+public class Point3d extends Point2d{
+    public static void main(String[] args) {
+        Point3d myPoint = new Point3d ();//создает точку (0.0,0.0,0.0)
+        Point3d myOtherPoint = new Point3d (5,3,3);//создает точку (5,3,3)
+        Point3d myOtherPoint3 = new Point3d (5,3,3);//создает точку (5,3,3)
+        Point3d myOtherPoint1 = new Point3d (3,2,1);//создает точку (5,3,3)
+        System.out.println(distanceTo(myOtherPoint1,myOtherPoint));
+        System.out.println(comparePoint3d(myOtherPoint,myOtherPoint3)+"=true");
+        System.out.println(comparePoint3d(myOtherPoint,myOtherPoint1)+"=false");
+    }
+    private double zCoord;
+    public static double distanceTo(Point3d s,Point3d g){
+        double D=Math.sqrt(Math.pow(s.getX() - g.getX(),2)+Math.pow(s.getY() - g.getY(),2)+Math.pow(s.getZ() - g.getZ(),2));
+        return D;
+    }
+    public Point3d ( double x, double y, double z) {
+        super(x,y);
+        zCoord = z;
+    }
+    public Point3d () {
+        this(0.0, 0.0,0.0);
+    }
+    public double getZ () {
+        return zCoord;
+    }
+    public void setZ (double val) {
+        zCoord = val;
+    }
+    public static boolean comparePoint3d (Point3d one,Point3d two){
+        int c1,c2,c3;
+        c1=Double.compare(one.getX(),two.getX());
+        c2=Double.compare(one.getY(),two.getY());
+        c3=Double.compare(one.getZ(),two.getZ());
+        return (c1==0 && c2==0 && c3==0);
+    }
+}
